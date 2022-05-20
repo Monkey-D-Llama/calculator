31ae4d08a17d9bcc00514e1771efad54dae048b6
Monkey-D-Llama <billy.ballsack69420@gmail.com>
2022-05-20 09:27:55 -0400

    added base js functions

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let first = 0;
+let second = 0;
+let buttons = document.querySelector('.buttons');
+buttons.style.backgroundColor = "lightGreen";
+
+function add (num1, num2){
+    return num1 + num2;
+}
+function subtract(num1, num2){
+    return num1 - num2;
+}
+function multiply(num1, num2){
+    return num1 * num2;
+}
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2){
+
+}
